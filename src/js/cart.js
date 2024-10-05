b0e5fb25b2bdcca1be57d35e4c8ea475881ddd60
kimah72 <min23005@byui.edu>
2024-10-04 23:00:22 -0600

    last change for me tonight updated my empty cart code

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,34 +2,41 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-
-  if (cartItems == false || cartItems.length === 0) {
-    document.querySelector(".product-list").innerHTML =
-      "<li>Your cart is empty</li>";
-  } else {
-    const cartArray = [].concat(cartItems);
-    document.querySelector("#cart-count").textContent = cartArray.length;
-    const htmlItems = cartArray.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  
+  // I revamped my empty cart error code - kim 10/4
+  if (checkIfCartIsEmpty(cartItems)) {
+    return;
   }
 
-  // pull count and update
+  function checkIfCartIsEmpty(cartItems) {
+    if (!cartItems || cartItems.length === 0) {
+      document.querySelector(".product-list").innerHTML =
+        "<li>Your cart is empty</li>";
+      document.querySelector("#cart-count").textContent = 0;
+      return true;
+    }
+    return false;
+  }
   
+  const cartArray = [].concat(cartItems);
+  document.querySelector("#cart-count").textContent = cartArray.length; // this is suppose to update the cart count but isn't? km
+  const htmlItems = cartArray.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li id="${item.Brand.Id}" class="cart-card divider">
-<a href="#" class="cart-card__image">
-<img src="${item.Image}" alt="${item.Name}" />
-</a>
-<a href="#">
-<h2 class="card__name">${item.Name}</h2>
-</a>
-<p class="cart-card__color">${item.Colors[0].ColorName}</p>
-<p class="cart-card__quantity">qty: 1</p>
-<p class="cart-card__price">$${item.FinalPrice}</p>
-<button type='button' class="remove_item" onclick="remove_li('${item.Brand.Id}')">X</button>
-</li>`;
+    <a href="#" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}" />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+    <button type='button' class="remove_item" onclick="remove_li('${item.Brand.Id}')">X</button>
+  </li>`;
 
   return newItem;
 }
