6cac2ab14dca560522fe8fab31640e2c76b69412
kimah72 <min23005@byui.edu>
2024-10-04 22:27:15 -0600

    This is the only thing we didn't finish figuring out in the stretch goals

--- src/js/productDetail.mjs
+++ src/js/productDetail.mjs
@@ -3,7 +3,10 @@ import { setLocalStorage } from "./utils.mjs";
 
 let product = {};
 
-export default async function productDetails(productId) {
+export default async function productDetails(productId, selector) {
+  // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  // once we have the product details we can render out the HTML
+  // add a listener to Add to Cart button
     product = await findProductById(productId);
     renderProductDetails();
     document.getElementById("addToCart").addEventListener("click", addToCart);
