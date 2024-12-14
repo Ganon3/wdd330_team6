3de89a962c6cc37e59109fdd60f5521b0bdd7bfd
mari <marianabarnett3@gmail.com>
2024-12-14 11:06:52 -0500

    add unit test and fixed cart count again

--- src/js/productDetail.mjs
+++ src/js/productDetail.mjs
@@ -9,7 +9,7 @@ export default async function productDetails(productId, selector) {
   // add a listener to Add to Cart button
     product = await findProductById(productId);
     renderProductDetails();
-    document.getElementById("addToCart").addEventListener("click", addToCart);
+    // document.getElementById("addToCart").addEventListener("click", addToCart);
     
   }
 
